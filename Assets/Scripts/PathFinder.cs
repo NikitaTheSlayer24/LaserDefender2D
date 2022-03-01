574c60d7b0b8b2c5d7a6672e5d7acf75260b2133
NikitaTheSlayer24 <nikita.fedorov124@gmail.com>
2022-03-01 23:03:25 +0700

    Added player boundaries, waves for enemies, SO for pathfing

---
+++ Assets/Scripts/PathFinder.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PathFinder : MonoBehaviour
+{
+    [SerializeField] private WaveConfigSO waveConfig;
+
+    private List<Transform> waypoints;
+
+    private int waypointIndex = 0;
+
+    private void Start()
+    {
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].position;
+    }
+
+    private void Update()
+    {
+        FollowPath();
+    }
+
+    private void FollowPath()
+    {
+        if (waypointIndex < waypoints.Count)
+        {
+            Vector3 targetPosition = waypoints[waypointIndex].position;
+            float delta = waveConfig.GetMoveSpeed() * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, delta);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,18 +4,39 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float paddingLeft;
+    [SerializeField] private float paddingRight;
+    [SerializeField] private float paddingTop;
+    [SerializeField] private float paddingBottom;
 
     private Vector2 _rawInput;
+    private Vector2 _minBounds;
+    private Vector2 _maxBounds;
+
+    private void Start()
+    {
+        InitBounds();
+    }
 
     private void Update()
     {
         Move();
     }
 
+    private void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        _minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        _maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
     private void Move()
     {
-        Vector3 delta = _rawInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = _rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, _minBounds.x + paddingLeft, _maxBounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, _minBounds.y + paddingBottom, _maxBounds.y - paddingTop);
+        transform.position = newPos;
     }
 
     private void OnMove(InputValue value)
